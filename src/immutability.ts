90b5c110e029a7613cb1ee1839bf94f827fdb78a
badger <badger@badger.local>
2021-11-22 11:34:44 +0200

    added tasks

---
+++ src/immutability.ts
@@ -0,0 +1,49 @@
+// // Задание 1
+// export type OriginalTeam = {
+//   size: number;
+//   name: string;
+//   league: string;
+// };
+
+// export type ExpectedTeam = {
+//   name: string;
+//   league: string;
+//   roster: number;
+// };
+
+export const originalTeamToExpectedTeam = (
+  originalTeam: OriginalTeam
+  // eslint-disable-next-line
+  // @ts-ignore
+): ExpectedTeam => {
+  //
+};
+
+// // Задание 2
+// type SomeArray = Array<number | string>;
+
+const originalArrayToExpectedArray = (
+  originalArray: SomeArray
+  // eslint-disable-next-line
+  // @ts-ignore
+): SomeArray => {
+  //
+};
+
+// // Задание 3
+
+// export type Team = {
+//   name: string;
+//   captain: {
+//     name: string;
+//     age: number;
+//   };
+// };
+
+export const originalTeamToExpectedTeam2 = (
+  originalTeam: Team
+  // eslint-disable-next-line
+  // @ts-ignore
+): Team => {
+  //
+};
