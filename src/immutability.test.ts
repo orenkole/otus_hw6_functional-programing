72b2f2a736c3974fe4ba58be06ca2f1eee04556c
badger <badger@badger.local>
2021-11-22 15:28:53 +0200

    some solutions

--- src/immutability.test.ts
+++ src/immutability.test.ts
@@ -1,6 +1,6 @@
 test("dummy test", () => undefined);
 
-/*
+
 
 import { OriginalTeam, ExpectedTeam, originalTeamToExpectedTeam, originalArrayToExpectedArray, originalTeamToExpectedTeam2 } from "./immutability";
 
@@ -18,7 +18,7 @@ test("team to team", () => {
     roster: 25,
   };
 
-  expect(originalTeamToExpectedTeam(originalTeam)).toBe(expectedTeam);
+  expect(originalTeamToExpectedTeam(originalTeam)).toStrictEqual(expectedTeam);
 });
 
 // Задание 2
@@ -27,7 +27,7 @@ test("array to array", () => {
 
   const expectedArray = ["two", 3, 4, 5];
 
-  expect(originalArrayToExpectedArray(originalArray)).toBe(expectedArray);
+  expect(originalArrayToExpectedArray(originalArray)).toStrictEqual(expectedArray);
 });
 
 // Задание 3
@@ -48,7 +48,5 @@ test("team to team deep", () => {
     },
   };
 
-  expect(originalTeamToExpectedTeam2(originalTeam)).toBe(expectedTeam);
+  expect(originalTeamToExpectedTeam2(originalTeam)).toStrictEqual(expectedTeam);
 });
-
-*/
--- src/pureFunctions.ts
+++ src/pureFunctions.ts
@@ -1,23 +1,31 @@
 // // Задание 1
-// export type Team = { name: string; score: number };
+export type Team = { name: string; score: number };
 
 export const getTopName = (
   teams: Team[]
   // eslint-disable-next-line
   // @ts-ignore
 ): string => {
-  //
+  return teams.reduce((prev, current) => {
+    return prev.score > current.score ? prev : current;
+  }).name;
 };
 
 // // Задание 2
-// export type QsObj = Record<string, string | number | boolean | object>;
+export type QsObj = Record<string, string | number | boolean | object>;
 
 export const createQs = (
   qsObj: QsObj
   // eslint-disable-next-line
   // @ts-ignore
 ): string => {
-  //
+  return "?".concat(
+    Object.entries(qsObj)
+      .map(([key, value]) => {
+        return `${key}=${value}`;
+      })
+      .join("&")
+  );
 };
 
 // // Задание 3
@@ -27,5 +35,10 @@ export const parseQs = (
   // eslint-disable-next-line
   // @ts-ignore
 ): QsObj => {
-  //
+  return Object.fromEntries(
+    qs
+      .substring(qs.indexOf("?") + 1)
+      .split("&")
+      .map((item) => item.split("="))
+  );
 };
