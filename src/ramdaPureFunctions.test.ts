9e6e4c3b45a5da1766924e5103c2f5cec516559e
badger <badger@badger.local>
2021-11-26 15:31:30 +0200

    hw6: functional programming. ramda tests

--- src/ramdaPureFunctions.test.ts
+++ src/ramdaPureFunctions.test.ts
@@ -9,6 +9,17 @@ import {
   parseQs,
 } from "./ramdaPureFunctions";
 
+test("getTopName", () => {
+  const teams: Team[] = [
+    { name: "Lions", score: 5 },
+    { name: "Tigers", score: 4 },
+    { name: "Bears", score: 6 },
+    { name: "Monkeys", score: 2 },
+  ];
+
+  expect(getTopName(teams)).toBe("Bears");
+});
+
 test("createQs", () => {
   const qsObj: QsObj = {
     page: "2",
@@ -25,7 +36,7 @@ test("createQs", () => {
 test("parseQs", () => {
   const qs = "?page=2&pageSize=10&total=205&somethingElse=value";
 
-  expect(parseQs(qs)).toBe({
+  expect(parseQs(qs)).toEqual({
     page: "2",
     pageSize: "10",
     total: "205",
